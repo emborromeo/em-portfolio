5146f10fd49a36025b34b42964f589a74d9735f3
emborromeo <ericaborromeo06@gmail.com>
2021-01-18 16:32:52 +0800

    chages

--- main.js
+++ main.js
@@ -3,4 +3,9 @@ function changeImg(x){
 }
 function backImg(x){
     x.src="images/pic.png";
+
 }
+$('.video-btn').on('click', function(e){
+    e.preventDefault();
+    $( '#' + $(this).data('modal-id') ).modal();
+});
